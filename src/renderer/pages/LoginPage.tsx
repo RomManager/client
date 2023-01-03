307d744fd6a23b5323729e293b63263a39544bc9
Valentin Zwerschke <vallezw@gmail.com>
2023-01-03 17:32:21 +0100

    Configure tailwind gradient

--- src/renderer/pages/LoginPage.tsx
+++ src/renderer/pages/LoginPage.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>
-      <h1 className="font-sans text-blue-1 text-5xl">LoginPage</h1>
+    <div className="bg-gradient-to-tr from-blue-0 to-pink-0 h-screen place-content-center grid">
+      <div className="bg-gray-1 flex p-2 rounded-lg shadow-2xl">
+        <h1 className="font-sans font-extrabold text-2xl text-white">
+          RomManager
+        </h1>
+      </div>
     </div>
   )
 }
